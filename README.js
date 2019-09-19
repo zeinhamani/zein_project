89364c216a03cdc967acd193faa01c42e8ca924c
zeinhamani <55551231+zeinhamani@users.noreply.github.com>
2019-09-19 23:55:14 +0200

    Ajout un peut de java script

--- README.js
+++ README.js
@@ -2,3 +2,22 @@
 Projet d'éssai
 
 Donc on va commencer
+function send() {
+    var body = {value: document.getElementById("value").value};
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+          var response = JSON.parse(this.responseText);
+          document.getElementById("result").innerHTML = response.postData.text;
+      }
+  };
+  request.open("POST", "https://mockbin.com/request");
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send(JSON.stringify(body));
+} 
+const form = document.getElementById("form");
+form.addEventListener('submit',function(event){
+  event.preventDefault();
+  send();
+  
+});
